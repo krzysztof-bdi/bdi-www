f9acab108bb2497502add45bc74bb345422702f6
Krzysztof Rek <krzysztof@baltic-digital.org>
2025-10-17 01:42:07 +0200

    Fix build: export geistSans + lato

--- app/(site)/fonts.ts
+++ app/(site)/fonts.ts
@@ -1,2 +1,16 @@
+import localFont from "next/font/local"
 import { Lato } from "next/font/google"
-export const lato = Lato({ subsets:["latin"], weight:["300","400","700"], variable:"--font-sans", display:"swap" })
+
+export const geistSans = localFont({
+  src: "../../public/fonts/GeistVF.woff2",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+  display: "swap",
+})
+
+export const lato = Lato({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--font-sans",
+  display: "swap",
+})
